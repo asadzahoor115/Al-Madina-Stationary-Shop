64bdf6667fd2c65095fd326c1da4a419c63c1576
asadzahoor115 <asadzahoor115@gmail.com>
2026-07-05 15:25:04 +0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Al-Madina-Stationary-Shop/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
